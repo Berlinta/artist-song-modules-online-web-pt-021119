fba726e84396e87c6a670d8a00f2fec1d0713ee8
Nash Brancic <nashfft@gmail.com>
2019-04-02 01:00:57 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,5 +1,20 @@
 module Memorable
   
-  
+  def reset_all
+    all.clear
+  end
+
+  def count
+    all.count
+    end
+  end
+
+  module InstanceMethods
+
+    def initialize
+      self.class.all << self
+    end
+
+  end
   
 end
