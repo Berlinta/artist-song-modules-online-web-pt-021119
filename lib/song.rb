39880a3eb44b59c1eb6675422ede1e84a36f6e9b
Nash Brancic <nashfft@gmail.com>
2019-04-02 01:26:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,7 @@ require 'pry'
 class Song
   extend Findable::ClassMethods
   extend Memorable::ClassMethods
+  include Paramable::InstanceMethods
   include Memorable::InstanceMethods
   attr_accessor :name
   attr_reader :artist
